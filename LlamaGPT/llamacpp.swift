8f2f03fc786112ba05aac329bf971e0ef7f5f776
Varun Kohli <kohli.varun5@gmail.com>
2023-09-16 19:37:00 -0400

    .

--- LlamaGPT/llamacpp.swift
+++ LlamaGPT/llamacpp.swift
@@ -1,9 +1,9 @@
-//
-//  llamacpp.swift
-//  LlamaGPT
-//
-//  Created by Varun Kohli on 9/16/23.
-//
+  //
+  //  llamacpp.swift
+  //  LlamaGPT
+  //
+  //  Created by Varun Kohli on 9/16/23.
+  //
 
 import Foundation
 import llama
@@ -11,76 +11,54 @@ import llama
 
 /*
  
-@Sendable func run() async {
-  while true {
-    print("Enter prompt: ")
-    guard let prompt = readLine()?.trimmingCharacters(in: .whitespacesAndNewlines), !prompt.isEmpty else {
-      break
-    }
-    
-    let tokenStream = LlamaRunner(modelURL: url).run(
-      with: prompt,
-      stateChangeHandler: { state in
-        switch state {
-        case .notStarted:
-          break
-        case .initializing:
-          print("Initializing model... ", terminator: "")
-        case .generatingOutput:
-          print("Done.")
-          print("")
-          print("Generating output...")
-          print("\"", terminator: "")
-        case .completed:
-          print("\"")
-          print("")
-        case .failed:
-            // Handle this in the catch {}
-          break
-        }
-      })
-    
-    do {
-      for try await token in tokenStream {
-        print(token, terminator: "")
-      }
-    } catch let error {
-      print("")
-      print("Failed to generate output:", error.localizedDescription)
-    }
-  }
-}
-
+ @Sendable func run() async {
+ while true {
+ print("Enter prompt: ")
+ guard let prompt = readLine()?.trimmingCharacters(in: .whitespacesAndNewlines), !prompt.isEmpty else {
+ break
+ }
+ 
+ let tokenStream = LlamaRunner(modelURL: url).run(
+ with: prompt,
+ stateChangeHandler: { state in
+ switch state {
+ case .notStarted:
+ break
+ case .initializing:
+ print("Initializing model... ", terminator: "")
+ case .generatingOutput:
+ print("Done.")
+ print("")
+ print("Generating output...")
+ print("\"", terminator: "")
+ case .completed:
+ print("\"")
+ print("")
+ case .failed:
+ // Handle this in the catch {}
+ break
+ }
+ })
+ 
+ do {
+ for try await token in tokenStream {
+ print(token, terminator: "")
+ }
+ } catch let error {
+ print("")
+ print("Failed to generate output:", error.localizedDescription)
+ }
+ }
+ }
+ 
  */
 
 class ModelRunner {
   private var modelRunner : LlamaRunner?
   
-  private func userHomePath() -> String   {
-    let pw = getpwuid(getuid())
-    
-    if let home = pw?.pointee.pw_dir {
-      return FileManager.default.string(
-        withFileSystemRepresentation: home,
-        length: Int(strlen(home)))
-    }
-    
-    fatalError()
-  }
-  
-  private func userHome() -> URL   {
-    URL(fileURLWithPath: userHomePath(), isDirectory: true)
-  }
-  
-  
-  
- private func modelUrl() -> URL? {
-    return NSBundle.mainBundle().URLForResource("ggml-model-f16", withExtension: "gguf")
-      .appendingPathComponent("Github")
-      .appendingPathComponent("llama")
-      .appendingPathComponent("llama-2-7b-chat")
-      .appendingPathComponent("ggml-model-f16.gguf")
-    // TODO : Make this from bundle
+  private func modelUrl() -> URL? {
+    return Bundle.main.url(forResource: "llama-2-7b-chat", withExtension: "gguf", subdirectory: "models")
+      // TODO : Make this from bundle
   }
   
   func run(prompt:String) -> AsyncThrowingStream<String, Error> {
